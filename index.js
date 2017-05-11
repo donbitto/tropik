7eb9156f6ecd6daa01711bb344e08af15644170c
Pedro Pinho <pedro.pinho@itcenter.com.pt>
2017-05-11 16:27:31 +0100

    bugfix on server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 
-var server = http.createServer(fnction (requsest, response){
+var server = http.createServer(function (request, response){
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World\n");
 });
