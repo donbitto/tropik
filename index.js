3e5fa406c664b8484ab5cffd54aae38c15818e68
Pedro Pinho <pedro.pinho@itcenter.com.pt>
2017-05-11 16:24:18 +0100

    add basic server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+var http = require('http');
+
+
+var server = http.createServer(fnction (requsest, response){
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World\n");
+});
+
+server.listen(8080);
+
+console.log("Server running");
