1d496507fd2837a8601a9144362f37d26f86e027
Pedro Pinho <pedro.pinho@itcenter.com.pt>
2017-05-16 16:16:51 +0100

    add vue framework

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+var path = require('path');
 var express = require('express');
+var expressVue = require('express-vue');
 var app = express();
+
+app.engine('vue',expressVue);
+app.set('view engine', 'vue');
+app.set('views', path.join(__dirname,'/views'));
+
+app.set('vue',{
+    componentsDir: path.join(__dirname,'/views/components'),
+    defaultLayout:'layout'
+});
 
 
 app.get('/', function(req,res){
-    res.send('Hello World!')
+    var scope = {
+        data :{
+            title: "Hello World",
+            message: 'Hello!',
+        },
+        vue :{
+            head:{
+                title:"Hello!",
+            },
+            components:['message']
+        }
+       
+    };
+    res.render('index',scope);
 });
 
 app.listen(8080, function(){
---
+++ views/index.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <div>
+        <h1></h1>
+        <P>Welcome to the  demo.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
